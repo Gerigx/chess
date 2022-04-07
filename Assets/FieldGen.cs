b77f3819a000e0a7f9358307257d8de7bc6f28f2
Gerigx <Gerigx@users.noreply.github.com>
2022-04-07 11:54:58 +0200

    Test

---
+++ Assets/FieldGen.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGen : MonoBehaviour
+{
+    
+    [SerializeField] GameObject white, black;
+
+    string[] sAlpha = new string[8] { "A", "B", "C", "D", "E", "F", "G", "H"};
+
+    int counter;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //white.name = "";
+        //black.name ="";
+
+        Generation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Generation()
+    {
+        
+        for(int j = 0; j < 8; j++)
+        {
+            if(j%2==0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if(i%2==0)
+                    {
+                        Debug.Log("Counter: " + counter + " Buchstabe: " + sAlpha[j] + " Spalte: " + i + " Kontrolle: " + j );
+                    white.name = sAlpha[j] + i;
+                    Instantiate(white, new Vector2(i,j), Quaternion.identity);  
+                    
+
+                    counter ++;
+                    //Debug.Log(counter + " " + sAlpha[j] + "" + i);
+
+                    }else if(i%2!=0)
+                    {
+                        Debug.Log("Counter: " + counter + " Buchstabe: " + sAlpha[j] + " Spalte: " + i + "Kontrolle: " + j );
+                        black.name = sAlpha[j] + i;
+                        Instantiate(black, new Vector2(i,j), Quaternion.identity);
+                        
+
+                                            counter ++;
+                   //Debug.Log(counter + " " + sAlpha[j] + "" + i);;
+                    }            
+                }
+            }else if(j%2!=0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    
+                     if(i%2!=0)
+                    {
+                        Debug.Log("Counter: " + counter + " Buchstabe: " + sAlpha[j] + " Spalte: " + i + "Kontrolle: " + j );
+                        white.name = sAlpha[j] + i;
+                        Instantiate(white, new Vector2(i,j), Quaternion.identity);
+                        
+                    counter ++;
+                    //Debug.Log(counter + " " + sAlpha[j] + "" + i);
+
+                    }else if(i%2==0)
+                    {
+                        Debug.Log("Counter: " + counter + " Buchstabe: " + sAlpha[j] + " Spalte: " + i + "Kontrolle: " + j );
+                    black.name = sAlpha[j] + i;
+                    Instantiate(black, new Vector2(i,j), Quaternion.identity);  
+                    
+                    counter ++;
+                    //Debug.Log(counter + " " + sAlpha[j] + "" + i);
+
+                    }
+                    
+           
+                }
+            }
+
+        }
+
+
+    }
+}
